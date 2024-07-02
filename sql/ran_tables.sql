4e493164a162fbb99007889c27fcf44ba92c3863
tdeslauriers <thomas.deslauriers@gmail.com>
2024-07-02 11:57:20 -0500

    fixed time issue

--- sql/ran_tables.sql
+++ sql/ran_tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE client (
     password VARCHAR(255) NOT NULL,
     name VARCHAR(64) NOT NULL,
     owner VARCHAR(64) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     enabled BOOLEAN NOT NULL,
     account_expired BOOLEAN NOT NULL,
     account_locked BOOLEAN NOT NULL
@@ -14,7 +14,7 @@ CREATE TABLE scope (
     scope VARCHAR(64) NOT NULL,
     scope_name VARCHAR(32) NOT NULL,
     description VARCHAR(64),
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     active BOOLEAN NOT NULL
 );
 CREATE INDEX idx_sevice_name ON scope(service_name);
@@ -23,7 +23,7 @@ CREATE TABLE client_scope (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     client_uuid CHAR(36) NOT NULL,
     scope_uuid CHAR(36) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP,
     CONSTRAINT fk_client_scope_xref_id FOREIGN KEY (client_uuid) REFERENCES client (uuid),
     CONSTRAINT fk_scope_client_xref_id FOREIGN KEY (scope_uuid) REFERENCES scope (uuid)
 );
@@ -35,7 +35,7 @@ CREATE TABLE refresh (
     service_name VARCHAR(32),
     refresh_token CHAR(36) NOT NULL,
     client_uuid CHAR(36) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     revoked BOOLEAN NOT NULL
 );
 CREATE UNIQUE INDEX idx_refreshindex ON refresh(refresh_index);
